10d8e3141144bdb229767e4e02c97fe9bb798b5d
Oliver Day <oliver.day.swe@gmail.com>
2023-07-20 16:54:05 -0700

    feat: add 'Post' component

--- src/AllPosts.tsx
+++ src/AllPosts.tsx
@@ -1,6 +1,72 @@
-import { useParams } from "react-router-dom";
+import { useContext } from "react";
+import { Link, useParams } from "react-router-dom";
+import { UserContext } from "./App";
+import { GetPostsResponse } from "./database.types";
+import { usePostScore } from "./use-post-score";
+import { castVote } from "./cast-vote";
+import { UpVote } from "./UpVote";
+import { timeAgo } from "./time-ago";
 
 export default function AllPosts() {
   const { pageNumber } = useParams();
   return <h2>All Posts; page: {pageNumber}</h2>;
+}
+
+function Post({
+  postData,
+  myVote,
+  onVoteSuccess,
+}: {
+  postData: GetPostsResponse;
+  myVote: "up" | "down" | undefined;
+  onVoteSuccess: () => void;
+}) {
+  const score = usePostScore(postData.id, postData.score);
+  const { session } = useContext(UserContext);
+  return (
+    <div className="post-container">
+      <div className="post-upvote-container">
+        <UpVote
+          direction="up"
+          filled={myVote === "up"}
+          enabled={!!session}
+          onClick={async () => {
+            await castVote({
+              postId: postData.id,
+              userId: session?.user.id as string,
+              voteType: "up",
+              onSuccess: () => {
+                onVoteSuccess();
+              },
+            });
+          }}
+        />
+        <p className="text-center" data-e2e="upvote-count">
+          {score}
+        </p>
+        <UpVote
+          direction="down"
+          filled={myVote === "down"}
+          enabled={!!session}
+          onClick={async () => {
+            await castVote({
+              postId: postData.id,
+              userId: session?.user.id as string,
+              voteType: "down",
+              onSuccess: () => {
+                onVoteSuccess();
+              },
+            });
+          }}
+        />
+      </div>
+      <Link to={`/post/${postData.id}`} className="flex-auto">
+        <p className="mt-4">
+          Posted By {postData.username} {timeAgo((postData as any).created_at)}{" "}
+          ago
+        </p>
+        <h3 className="text-2xl">{postData.title}</h3>
+      </Link>
+    </div>
+  );
 }
